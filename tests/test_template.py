dcffbd9f3691e1f2ff822b472d52f0757011df0c
Tom Elliott <ipse@paregorios.org>
2025-10-18 11:44:43 -0400

    stub

---
+++ tests/test_template.py
@@ -0,0 +1,14 @@
+#
+# This file is part of pleiades_writing_systems
+# by Tom Elliott for the Institute for the Study of the Ancient World
+# (c) Copyright 2025 by New York University
+# Licensed under the AGPL-3.0; see LICENSE.txt file.
+#
+
+"""
+Test the CHANGEME module
+"""
+
+from pathlib import Path
+
+test_data_path = Path(__file__).parent / "data"
